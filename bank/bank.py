1df4be14c52c8acd218d5651269c270f81f04e62
Deem02 <Deem2002q@gmail.com>
2025-09-21 22:40:29 +0300

    create 3 class add 1 constructot

--- bank/bank.py
+++ bank/bank.py
@@ -7,19 +7,36 @@
 import uuid
 class Customer():
     def __init__(self, first_name, last_name, password):
-        self.account_id = uuid.uuid4()
+        self.customer_id = uuid.uuid4()
         self.first_name = first_name
         self.last_name = last_name
         self.password= password
+        self.accounts = []
           
         
     def __str__(self):
-        return f"Customer: {self.first_name} {self.last_name}, Account ID: {self.account_id}"
+        return f"Customer: {self.first_name} {self.last_name}, Customer ID: {self.customer_id}"
     
     
  # self.balance_checking =balance_checking
         # self.balance_savings = balance_savings     
    
+class BankAccount():
+    account_number_counter= 1000
+    def __init__(self, customer, balance=0):
+        self.account_number = BankAccount.account_number_counter
+        BankAccount.account_number_counter +=1
+        self.balance = balance
+        self.customer = customer
+        
+    
+class CheckingAccount(BankAccount):
+    def __init__(self):
+        pass
+    
+class SavingsAccount(BankAccount):
+    def __init__(self):
+        pass
     
     #BankAccount():
 if __name__ == '__main__' : 
