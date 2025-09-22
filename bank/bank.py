e730ed1de3336995a27cf9c799546f4f485f4283
Deem02 <Deem2002q@gmail.com>
2025-09-22 09:38:34 +0300

    monday standup

--- bank/bank.py
+++ bank/bank.py
@@ -21,24 +21,30 @@ class Customer():
  # self.balance_checking =balance_checking
         # self.balance_savings = balance_savings     
    
-class BankAccount():
-    account_number_counter= 1000
-    def __init__(self, customer, balance=0):
-        self.account_number = BankAccount.account_number_counter
-        BankAccount.account_number_counter +=1
-        self.balance = balance
-        self.customer = customer
+# class BankAccount():
+#     account_number_counter= 1000
+#     def __init__(self, customer, balance=0):
+#         self.account_number = BankAccount.account_number_counter
+#         BankAccount.account_number_counter +=1
+#         self.balance = balance
+#         self.customer = customer
         
     
-class CheckingAccount(BankAccount):
-    def __init__(self):
-        pass
+# class CheckingAccount(BankAccount):
+#     def __init__(self):
+#         pass
     
-class SavingsAccount(BankAccount):
-    def __init__(self):
-        pass
+# class SavingsAccount(BankAccount):
+#     def __init__(self):
+#         pass
     
     #BankAccount():
 if __name__ == '__main__' : 
-    customer1 = Customer('deem','alqasir', '123')
+    # customer1 = Customer('deem','alqasir', '123')
+    # print(customer1)
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    password = input("Password: ")
+    customer1 = Customer(first_name, last_name, password)
+    print(customer1)
     print(customer1)
