64f475da12e627339670b8441cc00572a7061f85
Deem02 <Deem2002q@gmail.com>
2025-09-27 17:48:51 +0300

    Edit test_bank

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -2,7 +2,7 @@ import unittest
 import tempfile
 import os
 from unittest.mock import patch
-from bank.bank import Customer, BankAccount, CheckingAccount, SavingsAccount, Bank
+from bank.bank import Customer, BankAccount, CheckingAccount, SavingsAccount, Bank, InvalidAmountError, InsufficientFundsError, OverdraftLimitError
 
 class TestCustomer(unittest.TestCase):
     def test_create_customer_object(self):
@@ -25,28 +25,31 @@ class TestBankAccountAndSubclass(unittest.TestCase):
         self.assertEqual(self.savings.balance, 500)
     
         
-    def test_deposit(self):
+    def test_deposit_valid(self):
         self.checking.deposit(100)
         self.assertEqual(self.checking.balance, 1600)
-        self.checking.deposit(-100)
-        self.assertEqual(self.checking.balance, 1600)
+       
+       
         
-        self.savings.deposit(10000)
-        self.assertEqual(self.savings.balance,10500)
-        self.savings.deposit(0)
-        self.assertEqual(self.savings.balance,10500)
+    def test_deposit_invalid_amount(self):
+        with self.assertRaises(InvalidAmountError):
+            self.checking.deposit(-100)  
+        self.assertEqual(self.checking.balance, 1500)
         
-        
-    def test_withdraw(self):
+        with self.assertRaises(InvalidAmountError):
+            self.savings.deposit(0)
+        self.assertEqual(self.savings.balance,500)
+          
+    def test_withdraw_vailed(self):
         self.checking.withdraw(50)
         self.assertEqual(self.checking.balance, 1450)
-        self.checking.withdraw(0)
-        self.assertEqual(self.checking.balance, 1450)
+
         
-        self.savings.withdraw(500)
-        self.assertEqual(self.savings.balance,0)
-        self.savings.withdraw(-150)
-        self.assertEqual(self.savings.balance,0)
+    def test_withdraw_insufficient_funds(self):
+        with self.assertRaises(InsufficientFundsError):
+            self.savings.withdraw(600)
+        self.assertEqual(self.savings.balance, 500) 
+
 
 class TestChekingAccountOverdraft(unittest.TestCase):     
     def setUp(self):
@@ -54,23 +57,27 @@ class TestChekingAccountOverdraft(unittest.TestCase):
         self.cheking = CheckingAccount(self.customer, balance=50.0)
         
     def test_first_overdraft(self):
-        self.assertTrue(self.cheking.withdraw(60)) 
+        with patch('sys.stdout'):
+            self.cheking.withdraw(60)
         self.assertEqual(self.cheking.balance, -45.0) 
         self.assertEqual(self.cheking.overdraft_count, 1)
         self.assertTrue(self.cheking.is_active)
         
     def test_overdraft_denied(self):   
-        self.assertFalse(self.cheking.withdraw(200)) 
+        with self.assertRaises(OverdraftLimitError):
+            with patch('sys.stdout'):
+                self.cheking.withdraw(200)
         self.assertEqual(self.cheking.balance, 50) 
         self.assertTrue(self.cheking.is_active)
         
     def test_second_overdraft(self):
         # self.test_first_overdraft()  # I can do it like this but each test should be isolated 
-        self.assertTrue(self.cheking.withdraw(60)) 
+        with patch('sys.stdout'):
+            self.cheking.withdraw(60)
         self.assertEqual(self.cheking.balance, -45.0) 
         self.assertEqual(self.cheking.overdraft_count, 1)
-        
-        self.assertTrue(self.cheking.withdraw(20))
+        with patch('sys.stdout'):
+            self.cheking.withdraw(20)
         self.assertEqual(self.cheking.overdraft_count, 2)
         self.assertFalse(self.cheking.is_active)
                  
@@ -85,7 +92,8 @@ class TestBank(unittest.TestCase):
         os.remove(self.test_file_path)
         
     def test_add_and_save_customer(self):
-        self.bank.add_customer('mohamed', 'alahmed', 'pass321', checking_balance=2000, savings_balance=200)
+        with patch('sys.stdout'):
+            self.bank.add_customer('mohamed', 'alahmed', 'pass321', checking_balance=2000, savings_balance=200)
         self.assertIn(1001,self.bank.customers)
         new_bank_instance = Bank(filename=self.test_file_path)
         self.assertEqual(len(new_bank_instance.customers), 1)
@@ -96,10 +104,12 @@ class TestBank(unittest.TestCase):
         self.assertEqual(loaded_customer.savings_account.balance, 200)
         
         
-    @patch('builtins.input', side_effect=[1001,'pass321'])    
-    def test_login(self,mock_input): 
-        self.bank.add_customer('mohamed', 'alahmed', 'pass321') 
-        self.bank.login() 
+    @patch('builtins.input')    
+    def test_login(self,mock_input):
+        with patch('sys.stdout'): 
+            self.bank.add_customer('mohamed', 'alahmed', 'pass321')
+            mock_input.side_effect=[1001,'pass321'] 
+            self.bank.login() 
         self.assertIsNotNone(self.bank.current_customer)
         self.assertEqual(self.bank.current_customer.account_id, 1001)
         self.assertEqual(self.bank.current_customer.first_name, 'mohamed')
